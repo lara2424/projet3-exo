e65439756526a7e1f621e9c031c980356d84821d
lara2401 <lara.kayser@student.uclouvain.be>
2021-02-16 11:01:05 +0100

    correctio strcpy

--- tp2/buf_strcpy.c
+++ tp2/buf_strcpy.c
@@ -3,14 +3,15 @@
 # include <string.h>
 char *buf_strcpy(const char *src) {
 
-if(src == NULL) return NULL;
+  if(src == NULL) return NULL;
 
-char* buff = NULL;
-buff = malloc(strlen(src)+1);
-if(buff == NULL) return NULL;
-for(int i =0; i < strlen(src);i++){
-  buff[i]=src[i];
-}
+  char* buff = malloc(sizeof(char)*(strlen(src)+1));
+
+  if(buff==NULL) return NULL;
+  
+  for(int i =0; i <= strlen(src);i++){
+    buff[i]=src[i];
+  }
 
 return buff;
 }
